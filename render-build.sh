0e7b050134d884ffceb740dcec9bbd0027f6b178
NKKNix <supawit.thongrueng@gmail.com>
2025-04-18 02:53:41 +0700

    feat: add superuser creation logic to render-build.sh after static file collection

--- render-build.sh
+++ render-build.sh
@@ -3,3 +3,14 @@
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --noinput
+python manage.py shell << END
+import os
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username=os.environ['DJANGO_SU_NAME']).exists():
+    User.objects.create_superuser(
+        os.environ['DJANGO_SU_NAME'],
+        os.environ['DJANGO_SU_EMAIL'],
+        os.environ['DJANGO_SU_PASSWORD']
+    )
+END
